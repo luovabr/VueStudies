e1e3b4e21a0584714366f303559ef1a0c813ae7d
luovabr <110795147+luovabr@users.noreply.github.com>
2022-09-23 17:08:51 -0300

    Add files via upload

---
+++ projeto-simples/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+      {{ titulo }}
+      <HelloWorld /> 
+      <button @click="titulo += '#'">Alterar</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    components: {
+        HelloWorld
+    },
+    data: function() {
+        return {
+            titulo: 'Teste Data usando Vue JS!!'
+        }
+    }
+}
+</script>
+
+<style>
+    #app {
+        background-color: chocolate;
+        color: #fff;
+    }
+</style>
