4b4408be25eafe3187567a29f7bce700deae9bbc
luovabr <110795147+luovabr@users.noreply.github.com>
2022-09-26 17:03:04 -0300

    Add files via upload

---
+++ componente-desafio/src/components/vuefooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <v-footer class="pa-3" app>
+      <v-spacer></v-spacer>
+      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
+  </div>
+</template>
