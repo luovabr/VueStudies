8058519cdc38f3e2d602808e96293824d646e242
luovabr <110795147+luovabr@users.noreply.github.com>
2022-09-26 17:04:24 -0300

    Delete src directory

--- src/components/vuefooter.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <v-footer class="pa-3" app>
-      <v-spacer></v-spacer>
-      <div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
-    </v-footer>
-  </div>
-</template>
