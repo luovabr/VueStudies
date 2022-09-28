8317e88339ffbc7da9462a6d831b4d9be6e3fa43
luovabr <110795147+luovabr@users.noreply.github.com>
2022-09-28 17:01:46 -0300

    Add files via upload

---
+++ citacoes/src/components/Citacao.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="citacao">
+        <slot name="default"></slot>
+        <slot name="fonte"></slot>
+        <slot name="texto"></slot>
+        <slot name="autor"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        frases: Array,
+        numero: Number
+    }
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+</style>
---
+++ citacoes/src/components/Citacoes.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        {{ numero }} {{ indice }}
+        <Citacao v-bind:frases="citacoes" v-bind:numero="indice">
+            <template v-slot:fonte>
+                <p>{{ citacoes[indice]["fonte"] }}</p>
+            </template>
+            <template v-slot:texto>
+                <p>{{ citacoes[indice]["texto"] }}</p>
+            </template>
+            <template v-slot:autor>
+                <p>{{ citacoes[indice]["autor"] }}</p>
+            </template>
+        </Citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from './Citacao.vue'
+
+export default {
+    components: {
+        Citacao
+    },
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
